62a8e6c6ff5c848ccba2a4b2419f0fe25fd0c157
Wangyh <wangyh@yeastar.com>
2018-02-09 15:20:07 +0800

    增加了一个Monster类

---
+++ src/StreamDemo.java
@@ -0,0 +1,9 @@
+import java.io.File;
+
+public class StreamDemo {
+    public static void main(String args[]){
+        File f = new File("E:\\StreamDemo");
+        System.out.println("file exits:"+f.exists());
+
+    }
+}
