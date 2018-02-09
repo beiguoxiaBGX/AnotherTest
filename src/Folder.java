4c574832b1222c61bf6e6a8f5bc5f5e21bc251e9
Wangyh <wangyh@yeastar.com>
2018-02-09 14:43:12 +0800

    增加了一个Monster类

---
+++ src/Folder.java
@@ -0,0 +1,8 @@
+import java.io.File;
+
+public class Folder {
+    public static void main(String args[]){
+        File f = new File("C:\\Windows");
+        System.out.println(f.getPath());
+    }
+}
